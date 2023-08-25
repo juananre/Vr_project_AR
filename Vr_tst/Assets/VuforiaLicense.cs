8dbbd025cf9dad5e6123c2849f2421e4b8a53206
juananre <juanandresferrerduenas@gmail.com>
2023-08-25 12:29:36 -0500

    holi

---
+++ Vr_tst/Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVdKSm9lbi8vLy8vQUFBQm1WUHFmdVFlVFU5OHZVMEQ3ZWt4QkhVc2t3YXgxNEFqeVFGb2RJVlRvUitGYjBzdytIcEVoUkdpcTBkMUtWeHNpRDhZSWM1N2NRRUk0YzZZdUM0ZXF1VElhMmN4QnJHa09LSFkwOGdLMkEvWHptSkozT2dMd1dKREwwWjFrZ1gweDJqRmc5bERoS0t6TTNsL2NFd1RnUVB6Y3FhNEg5bFZVREkxM0twTk9SV0hJeWlvbFE2MUl6aU5vUVBKSEFneVJWZWFjcStySDNzTktZditxbzN3Y0gxUVZGLzZyOHZuM1pNdE5oK2I1WHZPUGovcy9ianRQcVlHUzZ4K09QeUtmZ0VmL1A4U1pqZjhqSUducFI2MC9sWU5FNzVBQlZnaUVNbEROYzNZcUdXbVkwOWFjb09EWEVnY2YwWS8zcld0cTRWRTc0QUVnVkdkdkdqNUl4SGY3bTQvMjhmZDROUGM3QktraHJwYjNHUkQ=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
